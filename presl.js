e197d0a76fcfdb4fea1cc96c37135f29bc2d3e52
ArtSFernandes <108846532+ArtSFernandes@users.noreply.github.com>
2022-07-09 22:30:19 -0300

    Update presl.js

--- presl.js
+++ presl.js
@@ -10,7 +10,7 @@ const a = {
       "Conhece o \"Aplicativo Secreto\" que está ajudando as pessoas a descobrirem tudo na relação?"
   ],
   "falalink": "Certo, vou te contar! Clique no botão abaixo",
-  "link": "https://dominio.com"
+  "link": "https://parceirosamigospro.online/tkt2/"
 
 }
 let clock=document.querySelector("#clock");function getClock(){let a=new Date,b=String(a.getHours()).padStart(2,"0"),c=String(a.getMinutes()).padStart(2,"0");clock.innerText=`${b}:${c}`}function timeout(a){return new Promise(b=>setTimeout(b,a))}async function sleep(a,b,...c){return await timeout(a),b(...c)}getClock(),setInterval(getClock,1e3),document.addEventListener("DOMContentLoaded",()=>{let c=document.getElementById("name"),d=document.getElementById("image__header"),f=document.getElementById("message__inner");c.innerText+=a.nome,d.src=a.pfp;for(var g,b=0,e=a.primeiras.length;b<e;b++)setTimeout(async function(b){0==b?((g=document.createElement("div")).setAttribute("id",`message__${[b]}`),g.innerHTML+=`
